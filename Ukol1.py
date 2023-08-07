e17c1274a945b6e761a838093f44c1ace5cdf728
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-07 09:27:31 +0200

    Update Ukol1.py

--- Ukol1.py
+++ Ukol1.py
@@ -21,3 +21,16 @@ Desetinná čísla (Floats)
 Ntice (Tuples)
 Množiny s pevnou hodnotou (Frozen sets)
 
+# • What is a difference between list and tuple? 
+#   Jaký je rozdíl mezi list a tuple?
+
+#Hlavní rozdíl mezi seznamem (list) a nticí (tuple) v Pythonu je, že seznamy jsou měnitelné (mutable), 
+#zatímco n-tice jsou neměnitelné (immutable).
+#To znamená, že seznamy můžete měnit po jejich vytvoření: můžete přidávat, měnit nebo odstraňovat prvky.
+
+#Na druhou stranu, n-tice, jakmile jsou vytvořeny, nemohou být změněny. 
+#Nemůžete přidat, změnit nebo odstranit prvky v n-tici bez vytvoření nové n-tice.
+
+#Díky této vlastnosti jsou n-tice obecně vhodné pro případy, kdy máte kolekci prvků, které se nemají měnit, 
+#zatímco seznamy jsou vhodné pro kolekce prvků, které budou pravděpodobně měněny.
+
