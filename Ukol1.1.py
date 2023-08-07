c7d7e812f13009b2967609a1f46f147617315694
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-07 11:11:40 +0200

    Update Ukol1.1.py

--- Ukol1.1.py
+++ Ukol1.1.py
@@ -93,3 +93,107 @@ Kdy je vhodné použít který typ metody?
 Instanční metody jsou vhodné, když potřebujete pracovat s instancemi a jejich atributy.
 Statické metody se používají, když chcete provádět nějakou funkci, která nezávisí na instančních ani třídních atributech.
 Třídní metody se často používají, když potřebujete pracovat s třídními atributy nebo když potřebujete vytvořit alternativní konstruktory.
+
+# • What are dunder/magic methods? Provide and briefly describe few of them. 
+# Co jsou to dunder/magické metody? Vyjmenujte a stručně popište pár z nich.
+
+#"Dunder" metody (zkratka pro "double underscore" metody) nebo také "magické" metody jsou speciální metody v Pythonu, které mají dvojité podtržítka na začátku i konci jejich názvů (např. __init__, __call__). Tyto metody umožňují implementovat a upravit vlastní chování určitých operátorů a funkcí pro vlastní třídy.
+
+Několik častých dunder/magických metod:
+
+__init__(self, ...):
+
+Konstruktor třídy. Volá se, když je vytvořena nová instance třídy.
+__str__(self):
+
+Vrací reprezentaci objektu jako řetězec. Volá se například při použití funkce print() na objektu.
+__repr__(self):
+
+#Vrací "oficiální" reprezentaci objektu. Ideálně by měla vracet řetězec, který, pokud by byl předán funkci eval(), vytvoří objekt stejného typu s týmiž daty.
+__add__(self, other):
+
+Umožňuje definovat vlastní chování pro operátor +.
+__eq__(self, other):
+
+Umožňuje definovat vlastní chování pro operátor ==.
+__len__(self):
+
+Vrací délku objektu, pokud má smysl. Volá se při použití funkce len() na objektu.
+__getitem__(self, key):
+
+Umožňuje přístup k položkám objektu pomocí hranatých závorek, např. obj[key].
+__setitem__(self, key, value):
+
+Umožňuje nastavení hodnoty položky pomocí hranatých závorek, např. obj[key] = value.
+__call__(self, ...):
+
+Umožňuje "volat" instanci třídy jako funkci.
+__del__(self):
+
+Destruktor, volá se, když je instance třídy zničena.
+Tyto metody poskytují mocnou možnost modifikace chování objektů a poskytují Pythonu jeho charakteristickou flexibilitu a dynamiku.
+
+
+#What are, and how would you implement private, protected, and public class attributes in Python? 
+What about class inheritance when using them?
+Co jsou a jak byste implementovali privátní, chráněné, a veřejné atributy třídy v Pythonu? 
+Co jejich použití znamená pro třídní dědičnost?
+
+V Pythonu je konvence pro definování privátních, chráněných a veřejných atributů třídy založena na prefixech:
+
+Veřejné atributy:
+
+Standardní atributy třídy bez jakýchkoli prefixů.
+
+Přístupné odkudkoli.
+
+Příklad: my_attribute
+
+python
+Copy code
+class MyClass:
+    def __init__(self):
+        self.my_attribute = "I'm public!"
+Chráněné atributy:
+
+Atributy s jedním podtržítkem jako prefix.
+
+Konvence naznačuje, že by neměly být přímo přistupovány mimo třídu, ale technicky jsou stále přístupné.
+
+Příklad: _my_protected_attribute
+
+python
+Copy code
+class MyClass:
+    def __init__(self):
+        self._my_protected_attribute = "I'm protected!"
+Privátní atributy:
+
+Atributy s dvojitým podtržítkem jako prefix.
+
+Python změní název těchto atributů, aby je skutečně "skryl" (name mangling).
+
+Tyto atributy nejsou snadno přístupné z vnějšího kódu, ale stále je možné je získat pokud víte, jak.
+
+Příklad: __my_private_attribute
+
+python
+Copy code
+class MyClass:
+    def __init__(self):
+        self.__my_private_attribute = "I'm private!"
+Dědičnost:
+
+Veřejné atributy: Dědí se a jsou volně přístupné v potomkovských třídách.
+
+Chráněné atributy: Dědí se a by měly být přístupné pouze v potomkovských třídách (ačkoli v Pythonu je stále technicky možné 
+je přistupovat odkudkoli).
+
+Privátní atributy: Dědí se, ale díky name manglingu nejsou přímo přístupné v potomkovských třídách (musíte znát změněný název).
+
+Je důležité si uvědomit, že v Pythonu je vše založeno na důvěře a konvencích. 
+I když můžete použít chráněné a privátní atributy, Python nesnaží skrýt je silně, jak je tomu v některých jiných jazycích. 
+Místo toho Python spoléhá na konvenci "jsme všichni dospělí zde" 
+a očekává se, že vývojáři budou respektovat hranice nastavené těmito konvencemi.
+
+
