dbcbfa0cf6d190ac7360145a96ba5601aafce5a5
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-07 09:21:29 +0200

    Update Ukol1.py

--- Ukol1.py
+++ Ukol1.py
@@ -1,1 +1,23 @@
+#Mutable / Immutable
+# • Name at least three mutable and immutable built-in types.
+#   Vyjmenuj alespoň tři “proměnlivé” (mutable) a “nezměnitelné” (immutable) vestavěné (built-in) typy.
+
+Řešení:
+#Proměnné typy jsou takové, které umožňují měnit obsah "na místě" (in-place). Na druhou stranu, nezměnitelné typy neumožňují změny 
+#existujících objektů, ale vždy, když je provedena změna, vytvoří se nový objekt.
+
+Proměnné (mutable) vestavěné typy v Pythonu:
+
+Seznamy (Lists)
+Slovníky (Dictionaries)
+Množiny (Sets)
+Byte Arrays
+Třídy (Classes) - pokud jsou definovány uživatelem, mohou být také měněny
+Nezměnitelné (immutable) vestavěné typy v Pythonu:
+
+Celá čísla (Integers)
+Desetinná čísla (Floats)
+Řetězce (Strings)
+Ntice (Tuples)
+Množiny s pevnou hodnotou (Frozen sets)
 
