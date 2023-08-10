e8e42e7c6921a790a5475a35221da22e712a1063
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-10 11:11:45 +0200

    Update Ukol1.py

--- Ukol1.py
+++ Ukol1.py
@@ -3,7 +3,6 @@
 # • Name at least three mutable and immutable built-in types.
 #   Vyjmenuj alespoň tři “proměnlivé” (mutable) a “nezměnitelné” (immutable) vestavěné (built-in) typy.
 
-Řešení:
 #Proměnné typy jsou takové, které umožňují měnit obsah "na místě" (in-place). Na druhou stranu, nezměnitelné typy neumožňují změny 
 #existujících objektů, ale vždy, když je provedena změna, vytvoří se nový objekt.
 
@@ -25,7 +24,6 @@ Množiny s pevnou hodnotou (Frozen sets)
 # • What is a difference between list and tuple? 
 #   Jaký je rozdíl mezi list a tuple?
 
-Reseni:
 #Hlavní rozdíl mezi seznamem (list) a nticí (tuple) v Pythonu je, že seznamy jsou měnitelné (mutable), 
 #zatímco n-tice jsou neměnitelné (immutable).
 #To znamená, že seznamy můžete měnit po jejich vytvoření: můžete přidávat, měnit nebo odstraňovat prvky.
