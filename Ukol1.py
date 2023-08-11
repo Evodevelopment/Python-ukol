68aa7e0ce641035de0d3dd428568ccbc8bd6a830
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-08-11 12:52:27 +0200

    Update Ukol1.py

--- Ukol1.py
+++ Ukol1.py
@@ -1,10 +1,10 @@
 #Mutable / Immutable
 
-# • Name at least three mutable and immutable built-in types.
-#   Vyjmenuj alespoň tři “proměnlivé” (mutable) a “nezměnitelné” (immutable) vestavěné (built-in) typy.
+# Name at least three mutable and immutable built-in types.
+# Vyjmenuj alespoň tři “proměnlivé” (mutable) a “nezměnitelné” (immutable) vestavěné (built-in) typy.
 
-#Proměnné typy jsou takové, které umožňují měnit obsah "na místě" (in-place). Na druhou stranu, nezměnitelné typy neumožňují změny 
-#existujících objektů, ale vždy, když je provedena změna, vytvoří se nový objekt.
+# Proměnné typy jsou takové, které umožňují měnit obsah "na místě" (in-place). Na druhou stranu, nezměnitelné typy neumožňují změny 
+# existujících objektů, ale vždy, když je provedena změna, vytvoří se nový objekt.
 
 Proměnné (mutable) vestavěné typy v Pythonu:
 
@@ -21,12 +21,12 @@ Desetinná čísla (Floats)
 Ntice (Tuples)
 Množiny s pevnou hodnotou (Frozen sets)
 
-# • What is a difference between list and tuple? 
-#   Jaký je rozdíl mezi list a tuple?
+# What is a difference between list and tuple? 
+# Jaký je rozdíl mezi list a tuple?
 
-#Hlavní rozdíl mezi seznamem (list) a nticí (tuple) v Pythonu je, že seznamy jsou měnitelné (mutable), 
-#zatímco n-tice jsou neměnitelné (immutable).
-#To znamená, že seznamy můžete měnit po jejich vytvoření: můžete přidávat, měnit nebo odstraňovat prvky.
+# Hlavní rozdíl mezi seznamem (list) a nticí (tuple) v Pythonu je, že seznamy jsou měnitelné (mutable), 
+# zatímco n-tice jsou neměnitelné (immutable).
+# To znamená, že seznamy můžete měnit po jejich vytvoření: můžete přidávat, měnit nebo odstraňovat prvky.
 
 #Na druhou stranu, n-tice, jakmile jsou vytvořeny, nemohou být změněny. 
 #Nemůžete přidat, změnit nebo odstranit prvky v n-tici bez vytvoření nové n-tice.
